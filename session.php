df4dd407343368afbb698453392716ec674dc585
Ivan Rezini <ivan_rezini@estudante.sc.senai.br>
2019-07-18 19:41:08 -0300

    ultimas colocaçao de campos na pagina inicio

--- session.php
+++ session.php
@@ -4,5 +4,15 @@ session_start();
 require_once 'configDB.php';
 if (isset($_SESSION['nomeUsuario'])){
     $nomeUsuario = $_SESSION["nomeUsuario"];
-    echo "Nome do Usuario $nomeUsuario";
+   // echo "Nome do Usuario $nomeUsuario";
+    $sql = $conexão->prepare("SELECT * FROM usuario WHERE " . "nomeusuario =  ?");
+    $sql->bind_param("s",$nomeUsuario);
+    $sql->execute();
+    $resultado = $sql->get_result();
+    $linha = $resultado->fetch_array(MYSQLI_ASSOC);
+    
+  //  $nomeUsuario = $linha["nomeUsuario"];
+    $nomeCompleto = $linha["nome"];
+    $email = $linha["email"];
+    $criado = $linha["criado"];
 }else    header ("location: index.php");
